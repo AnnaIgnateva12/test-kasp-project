e0df241c4075d1bfd97ee3f55797198a28c74101
AnnaIgnateva12 <anna.ignateva@simbirsoft.com>
2022-07-21 10:47:38 +0400

    prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { useState } from 'react';
-import './App.css';
-import BuyBlock from './components/buyBlock/BuyBlock';
-import Header from './components/header/Header';
-import List from './components/list/List';
-import MainText from './components/mainText/MainText';
+import { useState } from 'react'
+import './App.css'
+import BuyBlock from './components/buyBlock/BuyBlock'
+import Header from './components/header/Header'
+import List from './components/list/List'
+import MainText from './components/mainText/MainText'
 
 function App() {
-
   const [currency, setCurrency] = useState('GBP')
 
   return (
     <div className="App">
-      <div className='Main'>
+      <div className="Main">
         <Header setCurrency={setCurrency} />
         <MainText />
         <List />
@@ -19,7 +18,7 @@ function App() {
       <BuyBlock currency={currency} />
       <div style={{ height: '2000px' }} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/buyBlock/constans.js
+++ src/components/buyBlock/constans.js
@@ -1,57 +1,56 @@
 export const devices = [
-    { label: '3 Devices, 1 Year', value: '35.99' },
-    { label: '1 Device, 1 Year', value: '27.99' },
-    { label: '1 Device, 2 Years', value: '43.99' },
-    { label: '3 Devices, 2 Years', value: '63.99' },
-    { label: '5 Devices, 1 Year', value: '53.99' },
-    { label: '5 Devices, 2 Years', value: '71.99' },
+  { label: '3 Devices, 1 Year', value: '35.99' },
+  { label: '1 Device, 1 Year', value: '27.99' },
+  { label: '1 Device, 2 Years', value: '43.99' },
+  { label: '3 Devices, 2 Years', value: '63.99' },
+  { label: '5 Devices, 1 Year', value: '53.99' },
+  { label: '5 Devices, 2 Years', value: '71.99' },
 ]
 
 export const customStyles = {
-    option: styles => ({
-        ...styles,
-        color: '#1D1D1B',
-        padding: '10px',
-        marginTop: '10px',
-        fontSize: '14px',
-        fontWeight: '400',
-        backgroundColor: "#23D1AE",
-        "&:hover": {
-            backgroundColor: "#7FE3CE"
-        },
-        "&:checked": {
-            backgroundColor: "#7FE3CE"
-        }
-    }),
-    control: styles => ({
-        ...styles,
-        backgroundColor: '#23D1AE',
-            borderRadius: 'none',
-            boxShadow: 'none',
-        border: 'solid 2px #1D1D1B',
-        outline: 'none',
-        fontSize: '14px',
-        fontWeight: '400',
-        "&:hover": {
-            borderColor: "#1D1D1B",
-            outline: 'none'
-            }
-
-    }),
-    indicatorSeparator: styles => ({
-        ...styles,
-        display: 'none'
-    }),
-    dropdownIndicator: styles => ({
-        ...styles,
-        color: '#1D1D1B',
-    }),
-    menu: styles => ({
-        ...styles,
-        backgroundColor: '#23D1AE',
-        border: 'solid 2px #1D1D1B',
-        borderRadius: 'none',
-        margin: 0,
-        boxShadow: 'none',
-    }),
+  option: (styles) => ({
+    ...styles,
+    color: '#1D1D1B',
+    padding: '10px',
+    marginTop: '10px',
+    fontSize: '14px',
+    fontWeight: '400',
+    backgroundColor: '#23D1AE',
+    '&:hover': {
+      backgroundColor: '#7FE3CE',
+    },
+    '&:checked': {
+      backgroundColor: '#7FE3CE',
+    },
+  }),
+  control: (styles) => ({
+    ...styles,
+    backgroundColor: '#23D1AE',
+    borderRadius: 'none',
+    boxShadow: 'none',
+    border: 'solid 2px #1D1D1B',
+    outline: 'none',
+    fontSize: '14px',
+    fontWeight: '400',
+    '&:hover': {
+      borderColor: '#1D1D1B',
+      outline: 'none',
+    },
+  }),
+  indicatorSeparator: (styles) => ({
+    ...styles,
+    display: 'none',
+  }),
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    color: '#1D1D1B',
+  }),
+  menu: (styles) => ({
+    ...styles,
+    backgroundColor: '#23D1AE',
+    border: 'solid 2px #1D1D1B',
+    borderRadius: 'none',
+    margin: 0,
+    boxShadow: 'none',
+  }),
 }
